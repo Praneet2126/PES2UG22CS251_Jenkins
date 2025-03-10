1a5e9e726030f5e2f3da0d9313fcce2f9a8a5fdc
Praneet <praneetvkhp@gmail.com>
2025-03-10 15:39:25 +0530

    Initial

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout<<"Learning Jenkins at the speed of light";
+  return 0;
+}
