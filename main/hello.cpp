13be4f5d477088c27d0806a97401bb6a23b29e2b
Praneet <praneetvkhp@gmail.com>
2025-03-10 16:41:47 +0530

    Edit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"Wow this updated"<<endl;
+  cout<<"Wow this updated again!"<<endl;
   return 0;
 }
