c72b9f8401eb979c9ff17f34041f87facd728d45
Praneet <praneetvkhp@gmail.com>
2025-03-10 16:45:32 +0530

    ...

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins lmao\n";
   cout << "I have successfully built and run this  \n";
   cout<<"Wow this updated again!"<<endl;
   return 0;
